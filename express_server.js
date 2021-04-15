70d220648a388ef859df651a7bbb3f0ec97ed87d
Calvin <calvinchan87@gmail.com>
2021-04-15 01:03:30 +0000

    create registration page and GET route

--- express_server.js
+++ express_server.js
@@ -91,6 +91,13 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"]
+  };
+  res.render("urls_register", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
